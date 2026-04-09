18a4e0d73724a5f985efab06ada98c9047c01078
Philo Philip <philophilip21@gmail.com>
2026-04-09 20:27:25 +0530

    Project 1 - Personal Profile Page

---
+++ script.js
@@ -0,0 +1,13 @@
+// JS code for Words at Hero Section
+
+var typed = new Typed("#typing", {
+  strings: [
+    "Web Developer",
+    "Stock Trader",
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  backDelay: 1500,
+  loop: true
+});
+
